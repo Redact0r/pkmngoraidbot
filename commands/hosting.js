ff4e53f01b165a95b3c69d4cd0ad30af17f2f073
Tyler Sharp <johntylersharp@gmail.com>
2020-11-25 04:23:40 -0500

    same as last commit, minus a typo

--- commands/hosting.js
+++ commands/hosting.js
@@ -1,7 +1,7 @@
-const Discord = require("../node_modules/discord.js");
+const Discord = require("discord.js");
 
 module.exports = {
-  name: "host",
+  name: "hosting",
   description: "host a raid",
   execute(msg, args) {
     function getNickname(array) {
